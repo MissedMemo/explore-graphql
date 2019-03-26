14353388c64bd2c196623ee625b2686ff6e0dfef
AndyL <missedmemo@gmail.com>
2019-03-25 21:31:56 -0700

    attempt to query github from Apollo client

--- one-off examples/apollo-client-github-server/src/index.js
+++ one-off examples/apollo-client-github-server/src/index.js
@@ -1,7 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
+import ApolloClient, { gql } from 'apollo-boost'
 
 const App = () => <div>Hello, React!</div>
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/explore-graphql",
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer f918c540151c156956b6354940c30308ce88f36a`
+      }
+    })
+  }
+})
+
+client.query({
+  query: gql`
+    query getNameAndEmail {
+      viewer {
+        email
+        name
+      }
+    }
+  `
+})
+.then( res => console.log(res) )
+
 
 render(
   <App />,

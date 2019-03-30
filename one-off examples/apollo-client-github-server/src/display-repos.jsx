9156824c7c48b1fb0ec4341520cf01167e8bfa28
AndyL <missedmemo@gmail.com>
2019-03-29 20:59:34 -0700

    present add/remove star options to user

--- one-off examples/apollo-client-github-server/src/display-repos.jsx
+++ one-off examples/apollo-client-github-server/src/display-repos.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import AddStar from './add-star'
+import RemoveStar from './remove-star'
 
-export default ({data, numRepos, clickHandler}) => <>
-  <ul>
-    <h2>first {numRepos} repositories...</h2>
-    { data.viewer.repositories.edges.map(
-      ({node}) => <li key={node.name}>{ node.name }</li>
-    )}
-  </ul>
+export default ({data, numRepos, clickHandler, refetch}) => <>
+  <h2>first {numRepos} repositories...</h2>
+  { data.viewer.repositories.edges.map( ({node}) =>
+    <ul key={node.id}>
+      <li>{ node.name }</li>
+      {node.viewerHasStarred ? (
+        <RemoveStar id={node.id} refetch={refetch} />
+      ) : (
+        <AddStar id={node.id} refetch={refetch} />
+      )}
+      <li>stars {/*node.stargazers.totalCount*/}</li>
+    </ul>
+  )}
   <button onClick={clickHandler} >
     More...
   </button>
--- one-off examples/apollo-client-github-server/src/repos.jsx
+++ one-off examples/apollo-client-github-server/src/repos.jsx
@@ -19,14 +19,14 @@ const more = (getRepos, num) => {
 
 export default () => <div className="repos">
   <Query query={repoQuery} variables={{first: 5}}>
-    { ({loading, error, data, fetchMore }) => {
+    { ({loading, error, data, fetchMore, refetch }) => {
       if (loading) return <p>loading...</p>
       if (error) return <p>{ error.message }</p>
 
       const numRepos = data.viewer.repositories.edges.length
       const clickHandler = () => more(fetchMore, numRepos)
 
-      return <DisplayRepos {...{data, numRepos, clickHandler}} />
+      return <DisplayRepos {...{data, numRepos, clickHandler, refetch}} />
     }}
   </Query>
 </div>
